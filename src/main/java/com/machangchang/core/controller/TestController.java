67d3ba1f316965bb231748aa6071d6bf2cf099e7
machangchang <blemrr@gmail.com>
2018-09-09 02:43:31 +0800

    init commit

---
+++ src/main/java/com/machangchang/core/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.machangchang.core.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.machangchang.annotation.MyController;
+import com.machangchang.annotation.MyRequestMapping;
+import com.machangchang.annotation.MyRequestParam;
+
+@MyController
+@MyRequestMapping("/test")
+public class TestController {
+	
+
+	
+	 @MyRequestMapping("/doTest")
+    public void test1(HttpServletRequest request, HttpServletResponse response,
+    		@MyRequestParam("param") String param){
+ 		System.out.println(param);
+	    try {
+            response.getWriter().write( "doTest method success! param:"+param);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	 
+	 
+	 @MyRequestMapping("/doTest2")
+    public void test2(HttpServletRequest request, HttpServletResponse response){
+        try {
+            response.getWriter().println("doTest2 method success!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
